b8c8410eedc48084165dd8264ba115dd4e363ca0
Dylan Hawley <dylanthomashawley@gmail.com>
2025-08-02 14:53:13 -0400

    shorten prompt

--- src/dataset/prompt_generator.py
+++ src/dataset/prompt_generator.py
@@ -32,7 +32,7 @@ class PromptGenerator:
         )
         
     def enhance_prompt(self, text):
-        return f"This four-panel image showcases a transformation from a photorealistic image to a Minecraft skin; [TOP-LEFT] features our <subject>, which has the description {text}; [TOP-RIGHT] shows the left front up and right back up isometric positions of the <subject> as a Minecraft avatar; [BOTTOM-LEFT] displays the left back down and right front down isometric positions of the <subject> as a Minecraft avatar; [BOTTOM-RIGHT] is a complete uv map for the Minecraft skin file constructed from the isometric views of our <subject> laid out in the previous two panels."
+        return f"a four-panel image; [TOP-LEFT] features our <subject>, {text}; [TOP-RIGHT] shows isometric positions of the <subject> skin; [BOTTOM-LEFT] shows isometric positions of the <subject> skin; [BOTTOM-RIGHT] is a uv map for the Minecraft skin."
         
     def generate_prompt(self, text, output):
         enhanced_text = self.enhance_prompt(text)
